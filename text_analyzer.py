826b3d31e5b5ae723013068954638fbb1354f4ce
Premitium <simeonparvanov@ymail.com>
2017-04-03 10:44:32 +0300

    Added fixtures to add and remove file before and after tests

--- text_analyzer.py
+++ text_analyzer.py
@@ -7,7 +7,7 @@
    fixtures - code run before and/or after each test function
    assertions - specific tests for conditions and behaviors
  """
-
+import os
 import unittest
 
 """In test driven dev we write enough code to satisfy our test"""
@@ -16,6 +16,22 @@ def analyze_text():
 
 class TextAnalysisTests(unittest.TestCase):
     """Tests for the `analyze_text()`."""
+
+    def setUp(self):
+        """Fixture that creates a file for the text methods to use"""
+        self.filename = 'text_analysis_test_file.txt'
+        with open(self.filename, 'w') as f:
+            f.write('Now we are engaged in a great civil war.\n'
+                    'testign whether that nation,\n'
+                    'or any nation so conceived and dedicated,\n'
+                    'can long endure')
+
+    def tearDown(self):
+        """Fixture that deletes the files used by the test methods"""
+        try:
+            os.remove(self.filename)
+        except:
+            pass
 
     def test_function_runs(self):
         """
