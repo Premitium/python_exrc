e93b0d14fa7a69aee708e2855691ff172f8bb474
Premitium <simeonparvanov@ymail.com>
2017-04-05 13:29:53 +0300

    Nice tag builder with key word arguments

--- starargskwargs.py
+++ starargskwargs.py
@@ -21,3 +21,12 @@ def tag(name, **kwargs):
     print(name)
     print(kwargs)
     print(type(kwargs))
+
+def tag_real(name, **kwargs):
+    result = '<' + name
+    for key, value in kwargs.items():
+        result += ' {k}="{v}"'.format(k=key, v=str(value))
+    result += '>'
+    return result
+
+""">>> tag_real('img', src="monet.jpg", alt="Sunrise by Claude Monet", border=1)"""
